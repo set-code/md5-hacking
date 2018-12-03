82cc5ad04e2238d18978779d2b7f14e38c82b77e
set-code <40846767+set-code@users.noreply.github.com>
2018-12-03 20:51:43 +0500

    Update md5.py

--- md5.py
+++ md5.py
@@ -7,8 +7,6 @@ import mods
 print("\n")
 #202cb962ac59075b964b07152d234b70
 
-md5f = hashlib.md5(open('Password_list','rb').read()).hexdigest()
-
 sys.stdout.write(BOLD)
 sys.stdout.write(GREEN)
 print("********************************************************")
